083e982adf056bf2af7f0585a89a8218245a23f7
rasuljonadhamov <rasuljonadhamov@gmail.com>
2024-01-08 11:12:30 +0500

    fixed home directory

--- src/components/directory-item/DirectoryItem.jsx
+++ src/components/directory-item/DirectoryItem.jsx
@@ -1,10 +1,12 @@
+import { useNavigate } from "react-router-dom";
 import "./DirectoryItem.scss";
 
 const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
-
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+  const handleClick = () => navigate(route);
   return (
-    <div className="directory-item-container">
+    <div className="directory-item-container" onClick={handleClick}>
       <div
         className="background-image"
         style={{
